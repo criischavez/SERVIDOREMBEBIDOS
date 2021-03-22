cb45d2f5e4a74f082186dcb38bfdf3dcb7a13415
criischavez <80277018+criischavez@users.noreply.github.com>
2021-03-22 10:58:26 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -67,8 +67,9 @@ function LED1_Off(){
   }
 
   // called when a message arrives
+saludo=str
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	document.getElementById("sensor").innerHTML=message.payloadString;
+	document.getElementById("saludo").innerHTML=message.payloadString;
   }
   
