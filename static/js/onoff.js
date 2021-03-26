2d14113b47c749aa7c43ce110c5fa476e52108f6
criischavez <80277018+criischavez@users.noreply.github.com>
2021-03-25 22:49:59 -0500

    Update onoff.js

--- static/js/onoff.js
+++ static/js/onoff.js
@@ -1,23 +1,26 @@
 function ON_OFF(){
-	document.getElementById("On-Off").innerHTML="On";
- 
-	var a;
-  a=1
-  b=0
-  do{
-		
-	if (a and 1){
-    	document.getElementById("On-Off").innerHTML="Off"
-      message = new Paho.MQTT.Message("Off desde el Servidor");
-      message.destinationName = "cristina.chavez@unach.edu.ec/ser-dis";
-      client.send(message)
-	    ;}
-	else:{
-      document.getElementById("On-Off").innerHTML="On"
-      message = new Paho.MQTT.Message("On desde el Servidor");
-      message.destinationName = "cristina.chavez@unach.edu.ec/ser-dis";
-      client.send(message)
-	    ;}
-  b=b+1
-  }
-  while (b>=0)
+	document.getElementById("On-Off").innerHTML="On"; /*Para que indique On*/
+;}
+ var a;
+ a=0
+ b=0
+ do{
+
+if (a and 1){
+	document.getElementById("On-Off").innerHTML="Off";
+	a=a-1
+;}
+else:{
+ 	document.getElementById("On-Off").innerHTML="On";
+ 	a=a+1
+;}
+b=b+1
+	 
+      	message = new Paho.MQTT.Message("On-off desde el Servidor");
+      	message.destinationName = "cristina.chavez@unach.edu.ec/ser-dis";
+      	client.send(message)
+	 
+	
+	 
+}
+while (b>=0)
