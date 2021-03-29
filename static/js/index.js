f9ed643c40d3fa4851daecfde4b1e2c3f4dac049
criischavez <80277018+criischavez@users.noreply.github.com>
2021-03-29 10:08:03 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -75,6 +75,9 @@ function LED1_Off(){
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
 	document.getElementById("sensor").innerHTML=message.payloadString;
+	document.getElementById("boton").innerHTML=message.payloadString;
+	  
+	  
   }
 
 
