7f2e38d163b2957d13092619b136eb73cfe3e65a
criischavez <80277018+criischavez@users.noreply.github.com>
2021-04-11 23:32:53 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -20,13 +20,7 @@ function LED1_Off(){
 }
 
 
-function Registro(){	
-	message = new Paho.MQTT.Message("YES");
-   	message.destinationName = "cristina.chavez@unach.edu.ec/ser-dis";
-    	client.send(message);
-	onMessageArrived(msg)
-	document.getElementById("registro").innerHTML= msg.payloadString;
-}
+
 
 
 // Create a client instance
